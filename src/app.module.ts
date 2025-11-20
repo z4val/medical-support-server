582ccb645b38adbe8af894c20b6118085f900c9e
z4val <danielzavaletamego@gmail.com>
2025-11-19 20:55:17 -0500

    set database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { setDatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: setDatabaseConfig,
+    }),
+    UserModule],
   controllers: [],
   providers: [],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const setDatabaseConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => ({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DATABASE'),
+  // aditional
+  autoLoadEntities: configService.get<boolean>('DB_AUTOLOAD'),
+  synchronize: configService.get<boolean>('DB_SYNCH'),
+  logging: configService.get<boolean>('DB_LOG'),
+});
