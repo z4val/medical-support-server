87fae12df3eabf4a993b6a8ca15742b84c166228
z4val <danielzavaletamego@gmail.com>
2025-11-19 20:29:41 -0500

    config main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  app.enableCors({
+    origin: config.get<string>('FRONTEND_URL'),
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
