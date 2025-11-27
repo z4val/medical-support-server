261d5111f180c1e3878237a59f257452b7475304
z4val <danielzavaletamego@gmail.com>
2025-11-27 10:59:14 -0500

    import typeorm and export services

--- src/domain/indicators/indicators.module.ts
+++ src/domain/indicators/indicators.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { IndicatorsService } from './indicators.service';
 import { IndicatorsController } from './indicators.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Indicator } from './entities/indicator.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Indicator])],
   controllers: [IndicatorsController],
   providers: [IndicatorsService],
+  exports: [IndicatorsService],
 })
 export class IndicatorsModule {}
--- src/domain/patient/patient.module.ts
+++ src/domain/patient/patient.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
+import { Patient } from './entities/patient.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Patient])],
   controllers: [PatientController],
   providers: [PatientService],
+  exports: [PatientService],
 })
 export class PatientModule {}
