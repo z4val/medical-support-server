027c0ac290a1a17e077720aaaa766be51e7205c5
z4val <danielzavaletamego@gmail.com>
2025-12-18 13:48:46 -0500

    feat: ai response endpoint working

--- src/modules/ai/ai.controller.ts
+++ src/modules/ai/ai.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AiService } from './ai.service';
 
 @Controller('ai')
 export class AiController {
   constructor(private readonly aiService: AiService) {}
+
+  @Post('AI-response')
+  async getAIResponse(@Body('model') model?: string, promt?: string) {
+    return this.aiService.getResponse(model);
+  }
 }
--- src/modules/ai/ai.service.ts
+++ src/modules/ai/ai.service.ts
@@ -8,13 +8,18 @@ export class AiService {
 
   //Test the client by sending a simple chat completion request
   private async getModelResponse(
-    model: string,
-    prompt: string,
+    model?: string,
+    prompt?: string,
   ): Promise<AssistantMessage | null> {
     const response = await this.client.chat.send({
-      model: model || 'minimax/minimax-m2',
+      model: model || 'openai/gpt-oss-120b:free',
       messages: [
-        { role: 'user', content: prompt || 'Explain quantum computing' },
+        {
+          role: 'user',
+          content:
+            prompt ||
+            'Genera un ejemplo de diagnóstico médico general asistido para un médico real que está atendiendo a un paciente con posible TCA. Evita generar formatos de tablas o gráficos, limítate a texto plano de máximo 200 palabras.',
+        },
       ],
     });
 
@@ -26,4 +31,17 @@ export class AiService {
   }
 
   async generateMedicalPrompt() {}
+
+  async getResponse(model?: string) {
+    const diagnosis = await this.getModelResponse(model);
+    if (!diagnosis || !diagnosis.content) {
+      throw new Error('No response from OpenAI model');
+    }
+    return {
+      model: model || 'openai/gpt-oss-120b:free',
+      diagnosis: diagnosis.content,
+      confidence: 0.95,
+      prompt: '',
+    };
+  }
 }
